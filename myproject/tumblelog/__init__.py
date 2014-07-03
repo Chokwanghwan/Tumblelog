49fb2966582804730da2cb39922029bbfce4d159
Chokwanghwan <kwang8992@gmail.com>
2014-07-03 17:47:13 +0900

    오타 및 indent 수

--- myproject/tumblelog/__init__.py
+++ myproject/tumblelog/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.comfig["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
+app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 db = MongoEngine(app)
 
 if __name__ == '__main__':
-  app.run()
+	app.run()

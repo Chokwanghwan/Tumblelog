777a5030935afb7be525f4ed68660f756951e720
Chokwanghwan <kwang8992@gmail.com>
2014-07-03 17:47:00 +0900

    indent 수정

--- myproject/tumblelog/models.py
+++ myproject/tumblelog/models.py
@@ -2,26 +2,28 @@ import datetime
 from flask import url_for
 from tumblelog import db
 
+
 class Post(db.Document):
-  created_at = db.DateTimeField(default=datetime.tatetime.now, required=True)
-  title = db.StringField(max_length=255, required=True)
-  slug = db.StringFiled(max_length=255, require=True)
-  body = db.StringField(required=True)
-  comments = db.ListField(db.EmbeddedDocumentField('comment'))
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    title = db.StringField(max_length=255, required=True)
+    slug = db.StringField(max_length=255, required=True)
+    body = db.StringField(required=True)
+    comments = db.ListField(db.EmbeddedDocumentField('Comment'))
 
-  def get_absolute_url(self):
-    return url_for('post', kwargs={"slug":self.slug})
+    def get_absolute_url(self):
+        return url_for('post', kwargs={"slug": self.slug})
 
-  def __unicode__(self):
-    return self.title
+    def __unicode__(self):
+        return self.title
 
-  meta = {
-    'allow_inheritance': True,
-    'indexes': ['-created_at', 'slug'],
-    'ordering': ['-created_at']
+    meta = {
+        'allow_inheritance': True,
+        'indexes': ['-created_at', 'slug'],
+        'ordering': ['-created_at']
     }
 
+
 class Comment(db.EmbeddedDocument):
-  created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-  body = db.StringField(verbose_name="Comment", required=True)
-  author = db.StringField(verbose_name="Name", max_length=255, required=True)
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    body = db.StringField(verbose_name="Comment", required=True)
+    author = db.StringField(verbose_name="Name", max_length=255, required=True)

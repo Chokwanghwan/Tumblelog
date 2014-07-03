233b19b91de3fa257b047779e17c4a38e59495a8
Chokwanghwan <kwang8992@gmail.com>
2014-07-03 22:36:11 +0900

    오타 수정, 블로그화를 위한 deTailView 수

--- myproject/tumblelog/views.py
+++ myproject/tumblelog/views.py
@@ -1,6 +1,8 @@
+from flask.ext.mongoengine.wtf import model_form
 from flask import Blueprint, request, redirect, render_template, url_for
-from flask.views import MothodView
+from flask.views import MethodView
 from tumblelog.models import Post, Comment
+
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
@@ -10,9 +12,38 @@ class ListView(MethodView):
 		return render_template('posts/list.html', posts=posts)
 
 class DetailView(MethodView):
-	def get(self, slug):
-		post = Post.objects.get_or_404(slug=slug)
-		return render_template('posts/detail.html', post=post)
+
+    form = model_form(Comment, exclude=['created_at'])
+
+    def get_context(self, slug):
+        post = Post.objects.get_or_404(slug=slug)
+        form = self.form(request.form)
+
+        context = {
+            "post": post,
+            "form": form
+        }
+        return context
+
+    def get(self, slug):
+        context = self.get_context(slug)
+        return render_template('posts/detail.html', **context)
+
+    def post(self, slug):
+        context = self.get_context(slug)
+        form = context.get('form')
+
+        if form.validate():
+            comment = Comment()
+            form.populate_obj(comment)
+
+            post = context.get('post')
+            post.comments.append(comment)
+            post.save()
+
+            return redirect(url_for('posts.detail', slug=slug))
+
+        return render_template('posts/detail.html', **context)
 
 posts.add_url_rule('/', view_func=ListView.as_view('list'))
 posts.add_url_rule('/<slug>/', view_func=DetailView.as_view('detail'))

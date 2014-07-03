728ab5714c59ea9eb027de2dd749b6c55449b1a1
Chokwanghwan <kwang8992@gmail.com>
2014-07-03 18:38:08 +0900

    __init__.py에 blueprints 초기화

--- myproject/tumblelog/__init__.py
+++ myproject/tumblelog/__init__.py
@@ -7,5 +7,11 @@ app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 db = MongoEngine(app)
 
+def register_blueprints(app):
+	from tumblelog.views import posts
+	app.register_blueprint(posts)
+
+register_blueprints(app)
+
 if __name__ == '__main__':
 	app.run()
